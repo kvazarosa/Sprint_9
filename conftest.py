627299e8667abdceb56cf2675fcd73038c98aec7
Jenya Chabanov <kvazarosa@yandex.ru>
2025-08-05 15:28:30 +0300

    Add GitHub Actions workflow

--- conftest.py
+++ conftest.py
@@ -1,3 +1,33 @@
+# import pytest
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+# from data import Urls
+# import os
+
+# @pytest.fixture(scope="function")
+# def driver():
+#     chrome_options = Options()
+    
+#     if os.getenv('SELENOID_ENABLED') == 'true':
+#         # Настройки для Selenoid
+#         chrome_options.add_argument("--no-sandbox")
+#         chrome_options.add_argument("--disable-dev-shm-usage")
+#         chrome_options.add_argument("--headless")
+        
+#         driver = webdriver.Remote(
+#             command_executor='http://localhost:4444/wd/hub',
+#             options=chrome_options
+#         )
+#     else:
+#         # Локальные настройки
+#         chrome_options.add_argument("--window-size=1920,1200")
+#         chrome_options.add_argument("--start-maximized")
+#         driver = webdriver.Chrome(options=chrome_options)
+    
+#     driver.get(Urls.HOME_PAGE_URL)
+#     yield driver
+#     driver.quit()
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -16,7 +46,7 @@ def driver():
         chrome_options.add_argument("--headless")
         
         # Увеличиваем таймауты
-        RemoteConnection.set_timeout(90)
+        RemoteConnection.set_timeout(120)
         
         driver = webdriver.Remote(
             command_executor='http://localhost:4444/wd/hub',
