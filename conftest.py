fa5e396e8cd59a5ccfb886f86cc107302197e394
Jenya Chabanov <kvazarosa@yandex.ru>
2025-08-05 15:20:40 +0300

    Add GitHub Actions workflow

--- conftest.py
+++ conftest.py
@@ -4,25 +4,26 @@ from selenium.webdriver.chrome.options import Options
 from data import Urls
 import os
 
-
 @pytest.fixture(scope="function")
 def driver():
     chrome_options = Options()
-
+    
     if os.getenv('SELENOID_ENABLED') == 'true':
-        # Настройки для Selenoid в CI
+        # Настройки для Selenoid
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--headless")
+        
         driver = webdriver.Remote(
             command_executor='http://localhost:4444/wd/hub',
             options=chrome_options
         )
     else:
-        # Локальные настройки (как у вас было)
+        # Локальные настройки
         chrome_options.add_argument("--window-size=1920,1200")
         chrome_options.add_argument("--start-maximized")
         driver = webdriver.Chrome(options=chrome_options)
-
+    
     driver.get(Urls.HOME_PAGE_URL)
     yield driver
     driver.quit()
