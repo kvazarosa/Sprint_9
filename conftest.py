de411bde1356f855f498642f5565f409446b3cb9
Jenya Chabanov <kvazarosa@yandex.ru>
2025-08-06 14:09:18 +0300

    Add GitHub Actions workflow

--- conftest.py
+++ conftest.py
@@ -1,37 +1,3 @@
-# import pytest
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.remote.remote_connection import RemoteConnection
-# from data import Urls
-# import os
-
-# @pytest.fixture(scope="function")
-# def driver():
-#     chrome_options = Options()
-    
-#     if os.getenv('SELENOID_ENABLED') == 'true':
-#         chrome_options.add_argument("--no-sandbox")
-#         chrome_options.add_argument("--disable-dev-shm-usage")
-#         chrome_options.add_argument("--headless")
-#         chrome_options.add_argument("--disable-gpu")
-#         chrome_options.add_argument("--disable-extensions")
-        
-#         driver = webdriver.Remote(
-#             command_executor='http://localhost:4444/wd/hub',
-#             options=chrome_options
-#         )
-#     else:
-#         chrome_options.add_argument("--window-size=1920,1200")
-#         chrome_options.add_argument("--start-maximized")
-#         driver = webdriver.Chrome(options=chrome_options)
-    
-#     driver.implicitly_wait(15)  
-#     driver.set_page_load_timeout(40) 
-    
-#     driver.get(Urls.HOME_PAGE_URL)
-#     yield driver
-#     driver.quit()
-
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -43,8 +9,13 @@ def driver():
     
     if os.getenv('SELENOID_ENABLED') == 'true':
         # Конфигурация для Selenoid
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
+        options.set_capability("browserName", "chrome")
+        options.set_capability("browserVersion", "120.0")
+        options.set_capability("selenoid:options", {
+            "enableVNC": True,
+            "enableVideo": False
+        })
+        
         driver = webdriver.Remote(
             command_executor='http://localhost:4444/wd/hub',
             options=options
@@ -52,6 +23,7 @@ def driver():
     else:
         # Локальная конфигурация
         options.add_argument("--window-size=1920,1200")
+        options.add_argument("--start-maximized")
         driver = webdriver.Chrome(options=options)
     
     yield driver
